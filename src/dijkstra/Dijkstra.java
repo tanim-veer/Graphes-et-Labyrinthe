23aa0f7095b9c1b853750a92021a2c85cb1677da
Tanim <113085404+tanimvr@users.noreply.github.com>
2025-05-04 21:52:45 +0200

    Update Dijkstra.java

--- src/dijkstra/Dijkstra.java
+++ src/dijkstra/Dijkstra.java
@@ -2,8 +2,13 @@ package dijkstra;
 
 import graph.Graph;
 import graph.ShortestPath;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Dijkstra<T> implements ShortestPath<T> {
+
+	Map<T, Integer> distances = new HashMap<>();  // distances minimales
+	Map<T, T> predecesseurs = new HashMap<>();
 
 	@Override
 	public Distances<T> compute(Graph<T> g, T src, Animator<T> animator) throws IllegalArgumentException {
